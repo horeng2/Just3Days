4a05121b9f7b71d8696a699addd3cd59af748e81
Kim Seon-yeong <huski.a.a@gmail.com>
2022-06-29 22:00:41 +0900

    프로젝트 추가

---
+++ Just3Days/Just3Days/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Just3Days
+//
+//  Created by 서녕 on 2022/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Just3Days/Just3Days/Just3DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just3DaysApp.swift
+//  Just3Days
+//
+//  Created by 서녕 on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct Just3DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
