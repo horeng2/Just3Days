23639077c31dec0ceb025862555a9f6ab9d7e57c
horeng2 <huski.a.a@gmail.com>
2022-06-29 22:05:20 +0900

    feat: 열거형 DaysOfMissionSet 구현

---
+++ Just3Days/Just3Days/Model/DaysOfMissionSet.swift
@@ -0,0 +1,14 @@
+//
+//  DaysOfMissionSet.swift
+//  Just3Days
+//
+//  Created by 서녕 on 2022/06/29.
+//
+
+import Foundation
+
+enum DaysOfMissionSet {
+    case firstDay
+    case secondDay
+    case thirdDay
+}
