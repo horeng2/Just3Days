3e36e45df4824e6e44e40110187c06ee6448897e
horeng2 <huski.a.a@gmail.com>
2022-07-02 00:44:58 +0900

    feat: 미션뽑기 뷰 구현

--- Just3Days/Just3Days/View/PickMissionView.swift
+++ Just3Days/Just3Days/View/PickMissionView.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct PickMissionView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("미 션  뽑 기")
+                .font(.system(size: 32))
+                .fontWeight(.bold)
+                .padding(.top, 40)
+            ThreeDaysMissionView()
+            Image("pickButton")
+                .resizable()
+                .frame(width: 70, height: 70, alignment: .center)
+                .padding(.top)
+            Text("남은 기회: 5회")
+                .font(.system(size: 20))
+                .padding(.bottom, 40)
+        }
+        .background(ColorPalette.beige.rgb())
+    }
+}
+
+struct ThreeDaysMissionView: View {
+    var body: some View {
+        ZStack(alignment: .center) {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(ColorPalette.lightGray.rgb(), lineWidth: 3)
+                .background(ColorPalette.lightBeige.rgb())
+                .cornerRadius(20)
+                .padding(.top, 30)
+                .padding(.horizontal, 40)
+            VStack(spacing: 20) {
+                PickedMissionView(day: "시작하는 날", mission: "파란문제 뿌시기")
+                PickedMissionView(day: "벌써 반이나 지난 날", mission: "제리 똥치우기")
+                PickedMissionView(day: "끝나는 날", mission: "흑마도사 만렙찍기")
+            }
+        }
+    }
+}
+
+struct PickedMissionView: View {
+    let day: String
+    let mission: String
+    var body: some View {
+        Text(day)
+            .font(.system(size: 22))
+            .fontWeight(.bold)
+        Text(mission)
+            .font(.system(size: 18))
+    }
+}
+
