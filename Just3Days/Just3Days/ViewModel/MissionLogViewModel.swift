511ca6f962667a2a063d47913e9b1aa055e2357e
horeng2 <huski.a.a@gmail.com>
2022-07-14 21:50:55 +0900

    refactor: missionLog Key의 타입을 String으로 변경

--- Just3Days/Just3Days/ViewModel/MissionLogViewModel.swift
+++ Just3Days/Just3Days/ViewModel/MissionLogViewModel.swift
@@ -9,26 +9,36 @@ import Foundation
 import Combine
 
 class MissionLogViewModel: ObservableObject {
-    @Published var missionLog = [Date: Mission]()
+    @Published var missionLog = [String: Mission]()
     @Published var currntMissionSet = [DayOfMission: Mission]()
     let missionPresetViewModel = MissionPresetViewModel()
     
-    func endMissionSet() {
+    func saveMissionLog() {
         currntMissionSet.forEach { day, mission in
             self.missionLog[day.date] = mission
         }
         currntMissionSet.removeAll()
     }
     
-    func saveMissionSet(_ missions: [DayOfMission: Mission]) {
-        self.currntMissionSet = missions
+    func saveCurrntMissionSet(_ missions: [DayOfMission: Mission]) {
+        missions.forEach{ day, mission in
+            self.currntMissionSet[day] = mission
+        }
     }
     
-    func updateSuccess(_ mission: Mission, date: Date) {
+    func updateSuccess(_ mission: Mission, date: String) {
         self.missionLog[date]?.isSuccess = true
     }
     
-    func fetch() -> [DayOfMission: Mission] {
+    func fetchTodayMission(today: Date) -> Mission {
+        guard let mission = self.currntMissionSet.filter({ $0.key.date == today.toString() }).values.first else {
+            return Mission(title: "")
+        }
+        
+        return mission
+    }
+    
+    func fetchCurrentMissions() -> [DayOfMission: Mission] {
         var missionSet = [DayOfMission: Mission]()
         if currntMissionSet.isEmpty {
             DayOfMission.allCases.forEach { day in
@@ -37,7 +47,7 @@ class MissionLogViewModel: ObservableObject {
         } else {
             missionSet = currntMissionSet
         }
-        
-        return missionSet
+
+        return currntMissionSet
     }
 }
